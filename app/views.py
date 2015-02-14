848f2f19c4d4c8b723a5d627e8a714159002b103
wincelet <brookelfnichols@gmail.com>
2015-02-14 00:25:07 -0500

    another change

--- app/views.py
+++ app/views.py
@@ -3,4 +3,7 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'nickname' : 'bob'} #dummy user
+    return render_template('index.html',
+                           title='Home',
+                           user=user)
